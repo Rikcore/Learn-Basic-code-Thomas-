3e604a64342860db7f407d0e7d309e9774ab2e60
Cyprien Chamayou <cyprien.chamayou@gmail.com>
2018-03-19 12:27:27 +0100

    first exercice

--- app/src/main/java/com/rikcore/tutostagethomas/MainActivity.java
+++ app/src/main/java/com/rikcore/tutostagethomas/MainActivity.java
@@ -2,24 +2,43 @@ package com.rikcore.tutostagethomas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int bonneReponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        final EditText editTextReponse = findViewById(R.id.editTextReponse);
+        editTextReponse.setInputType(InputType.TYPE_CLASS_NUMBER);
         final TextView textViewTest = findViewById(R.id.textViewTest);
         Button buttonOk = findViewById(R.id.buttonOk);
+
+        bonneReponse = 48;
+
+
 
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textViewTest.setText("");
+                int reponseUtilisateur = Integer.valueOf(editTextReponse.getText().toString());
+                if (reponseUtilisateur == bonneReponse){
+                    Toast.makeText(MainActivity.this, "Bonne réponse", Toast.LENGTH_SHORT).show();
+                    textViewTest.setText("La bonne réponse est bien 48");
+                } else {
+                    Toast.makeText(MainActivity.this, "Faux enculé", Toast.LENGTH_SHORT).show();
+                    textViewTest.setText("Mauvaise réponse, essaye encore");
+                }
             }
         });
     }
